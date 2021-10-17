51d54054d0f2d038eb4534bea43c7b870f8979e3
北山小虎 <asdfqs@163.com>
2021-10-17 22:56:37 +0800

    add event

--- src/components/ScrollBar.tsx
+++ src/components/ScrollBar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import "./sb.css";
 interface ISBar {
     children?: any
@@ -6,18 +7,30 @@ interface ISBar {
 export default (props: ISBar) => {
     const {children} = props;
     const containerRef = React.useRef(null);
-    const viewRef = React.useRef(null);
+    const viewRef = React.useRef<HTMLDivElement>(null);
+    const [top, setTop] = React.useState(0);
+
+    const scrollHandler = (e: any) => {
+        const top = e.target.scrollTop;
+        setTop(top);
+    }
+    useEffect(() =>{
+        if (viewRef.current) {
+            viewRef.current.addEventListener("scroll", scrollHandler, true);
+        }
+    },[])
+
     return <div ref={containerRef} className="sb-container">
         <div ref={viewRef} className="sb-view">
             {children}
         </div>
-        <Bar></Bar>
+        <Bar top={top}></Bar>
     </div>
     return null
 }
 
 const Bar = (props: any)=>{
     return <div className="sb-bar-container">
-        <div className="sb-bar"></div>
+        <div className="sb-bar" style={{"top": props.top||10}}></div>
     </div>
 }
