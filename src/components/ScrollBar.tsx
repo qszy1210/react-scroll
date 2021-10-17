adbaed6da3b8308fdcf3ad1b25f3b21fd98e05cd
北山小虎 <asdfqs@163.com>
2021-10-17 23:25:27 +0800

    bar style

--- src/components/ScrollBar.tsx
+++ src/components/ScrollBar.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useEffect } from 'react';
+import getInnerHeight from './getInnerHeight';
 import "./sb.css";
 interface ISBar {
     children?: any
@@ -8,16 +9,34 @@ export default (props: ISBar) => {
     const {children} = props;
     const containerRef = React.useRef(null);
     const viewRef = React.useRef<HTMLDivElement>(null);
-    const [top, setTop] = React.useState(0);
+    const barContainerRef = React.useRef<HTMLDivElement>(null);
+    const [top, doSetTop] = React.useState(0);
+    const [barHeight, doSetBarHeight] = React.useState(10);
     const [isShow, setIsShow] = React.useState(false);
 
     const scrollHandler = (e: any) => {
-        const top = e.target.scrollTop;
-        setTop(top);
+        setBarHeight(e.target);
+        setTop(e.target);
         if (!isShow) {
             setIsShow(true);
         }
+    };
+
+    const setBarHeight = (el: any)=>{
+        const {clientHeight, scrollHeight} = el;
+        const barContainerHeight = getInnerHeight(barContainerRef.current);
+        const barHeight = clientHeight/scrollHeight * barContainerHeight;
+        doSetBarHeight(barHeight);
     }
+    const setTop = (el: any) => {
+        const {scrollTop, clientHeight, scrollHeight} = el;
+        const barContainerHeight = getInnerHeight(barContainerRef.current);
+        const barHeight = clientHeight/scrollHeight * barContainerHeight;
+        const top = scrollTop/(scrollHeight-clientHeight)*(barContainerHeight-barHeight);
+        doSetTop(top);
+    }
+
+
     useEffect(() =>{
         if (viewRef.current) {
             viewRef.current.addEventListener("scroll", scrollHandler, true);
@@ -28,16 +47,18 @@ export default (props: ISBar) => {
         <div ref={viewRef} className="sb-view">
             {children}
         </div>
-        {isShow ?
-            <Bar top={top}></Bar> :
-            null
+        {
+            <Bar
+            isShow={isShow}
+            top={top} barHeight={barHeight} ref={barContainerRef}></Bar>
         }
     </div>
     return null
 }
 
-const Bar = (props: any)=>{
-    return <div className="sb-bar-container">
-        <div className="sb-bar" style={{"top": props.top||10}}></div>
-    </div>
-}
+const Bar = React.forwardRef((props: any, ref: any) =>{
+        return <div className="sb-bar-container" ref={ref} style={{display: props.isShow?"block":"none"}}>
+            <div className="sb-bar" style={{"top": props.top||0, "height": props.barHeight}}></div>
+        </div>
+})
+
